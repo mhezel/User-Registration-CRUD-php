ba433b484ee28cce1a94e773ba7ffb915c8a3528
mhezel <mhezelkhan@gmail.com>
2022-09-14 11:37:59 +0800

    Include new css styling on to the index.php - style.css Add flexbox arrange div boxes and container

--- index.php
+++ index.php
@@ -7,28 +7,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-<section>
-    <div class="content-display">
-        <div class="index-login-signup">
-            <h4>SIGN UP</h4>
-        <p>Don't have an Account yet? Sign up Here!!</p>
-                <form action="includes/signup.inc.php" method="post">
-                 <input type="text" name="uid" placeholder="Username"><br>
-                <input type="password" name="pwd" placeholder="Password"><br>
-                <input type="password" name="pwdRepeat" placeholder="Repeat Password"><br>
-                <input type="text" name="email" placeholder="Email">
-                <br>
-                <button type="submit" name="submit">Sign Up</button></form>
-        </div>
-        <div class="index-login-login">
-            <h4>LOGIN</h4>
-            <form action="includes/login.inc.php" method="post">
-                <input type="text" name="uid" placeholder="Username"><br>
-                <input type="password" name="pwd" placeholder="Password"><br>
-                <button type="submit" name="submit">Log In</button>
-            </form>
-        </div>
-        </div>
-</section>
+    <div class="page-content">
+            <section class="container">
+                <div class="lines">
+                    <div>
+                        <h4>Register Form</h4>
+                        <p>Don't have an Account yet? Sign up Here!!</p>
+                        <form class="form-1" action="includes/signup.inc.php" method="post">
+                                <input type="text" name="uid" placeholder="Username"><br>
+                                <input type="password" name="pwd" placeholder="Password"><br>
+                                <input type="password" name="pwdRepeat" placeholder="Repeat Password"><br>
+                                <input type="text" name="email" placeholder="Email">
+                            <br>
+                            <button type="submit" name="submit">Sign Up</button></form>
+                    </div>
+
+                    <div class="index-login-login">
+                        <h4>Login Here</h4>
+                        <form class="form-2" action="includes/login.inc.php" method="post">
+                            <input type="text" name="uid" placeholder="Username"><br>
+                            <input type="password" name="pwd" placeholder="Password"><br>
+                            <button type="submit" name="submit">Log In</button></form>
+                    </div>
+                    </div>
+            </section>
+    </div>
 </body>
 </html>
